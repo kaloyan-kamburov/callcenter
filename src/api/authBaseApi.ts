6ccadeac6f650d5019fe26ffa1c0254155062274
Kaloyan Kamburov <kamburov.kaloyan@gmail.com>
2026-02-20 00:03:19 +0200

    wip

--- src/api/authBaseApi.ts
+++ src/api/authBaseApi.ts
@@ -1,7 +1,9 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "@/utils/axiosBaseQuery";
 
-const AUTH_API_BASE_URL = import.meta.env.VITE_AUTH_API_BASE_URL;
+const AUTH_API_BASE_URL =
+  import.meta.env.VITE_AUTH_API_BASE_URL ||
+  "https://revolutionlab-001-site32.anytempurl.com/api";
 
 export const authBaseApi = createApi({
   reducerPath: "authBaseApi",
--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,7 +1,9 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { axiosBaseQuery } from "@/utils/axiosBaseQuery";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://revolutionlab-001-site31.anytempurl.com/api";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
