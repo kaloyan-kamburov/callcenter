13ba701de8df89799a2277e02517f532e4a87c00
Kaloyan Kamburov <kamburov.kaloyan@gmail.com>
2026-02-22 08:50:54 +0200

    translation

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import AppRouter from "./router/AppRouter";
 import { Provider } from "react-redux";
@@ -6,13 +6,25 @@ import { store } from "./store";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./theme";
 import { Toaster } from "react-hot-toast";
+import { useTranslation } from "react-i18next";
 import "./i18n";
+
+function PageTitleSync() {
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    document.title = t("app.pageTitle");
+  }, [i18n.language, t]);
+
+  return null;
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <PageTitleSync />
         <Toaster position="bottom-center" />
         <AppRouter />
       </ThemeProvider>
